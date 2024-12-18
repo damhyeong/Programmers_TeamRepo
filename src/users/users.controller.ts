ff165f085219295d8318e996cf6f097ef0d8a70a
Kim-Hyunhee <tta_bi@naver.com>
2024-12-18 15:24:32 +0900

    fix: created_at 삭제, 상태 코드 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Req,
   Res,
+  HttpCode,
 } from '@nestjs/common';
 import { EmailCheckDto } from './dto/email-check.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -25,11 +26,16 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/email-check')
+  @HttpCode(HttpStatus.OK)
   @ApiBody({
     type: EmailCheckDto,
   })
   @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
+    status: HttpStatus.OK,
+    description: '사용 가능한 이메일일 때.',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
     description: '이미 존재하는 이메일 계정이 있을 때.',
   })
   async emailCheck(@Body() emailCheckDto: EmailCheckDto) {
@@ -39,7 +45,7 @@ export class UsersController {
     if (isAlreadySignup) {
       throw new HttpException(
         { message: '이미 존재하는 이메일 계정입니다.' },
-        HttpStatus.BAD_REQUEST,
+        HttpStatus.OK,
       );
     } else {
       return { message: '사용 가능한 이메일입니다.' };
@@ -74,8 +80,8 @@ export class UsersController {
     type: LoginDto,
   })
   @ApiResponse({
-    status : HttpStatus.CREATED,
-    example : "asdhlfjkhasdl%^%*&^%&*askldfjzcnmv^.....",
+    status: HttpStatus.CREATED,
+    example: 'asdhlfjkhasdl%^%*&^%&*askldfjzcnmv^.....',
   })
   async login(@Body() loginDto: LoginDto) {
     const { access_token } = await this.usersService.userLogin(loginDto);
@@ -88,7 +94,7 @@ export class UsersController {
     } else {
       throw new HttpException(
         { message: '로그인 실패했습니다.' },
-        HttpStatus.BAD_REQUEST,
+        HttpStatus.UNAUTHORIZED,
       );
     }
   }
