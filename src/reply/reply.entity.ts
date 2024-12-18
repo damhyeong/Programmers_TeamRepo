3e8ece12bee025e0511c282b4db787a9e87ef750
Kim-Hyunhee <tta_bi@naver.com>
2024-12-18 11:53:02 +0900

    bug: entity 수정

--- src/reply/reply.entity.ts
+++ src/reply/reply.entity.ts
@@ -49,7 +49,7 @@ export class Reply {
   post: Posts;
 
   @ManyToOne(() => Reply, (reply) => reply.reply, {
-    nullable: false,
+    nullable: true,
     onDelete: 'NO ACTION',
     onUpdate: 'NO ACTION',
   })
