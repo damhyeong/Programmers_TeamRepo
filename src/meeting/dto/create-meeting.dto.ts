734cd83dbbe9c788ccbfc69db3a49cbd5ff04326
Kim-Hyunhee <tta_bi@naver.com>
2024-12-20 17:39:05 +0900

    fix: 컬럼 수정

--- src/meeting/dto/create-meeting.dto.ts
+++ src/meeting/dto/create-meeting.dto.ts
@@ -37,14 +37,4 @@ export class MeetingDTO {
   @IsDateString()
   @IsOptional()
   end_date: Date;
-
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  gender_condition: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  age_condition: string;
 }
